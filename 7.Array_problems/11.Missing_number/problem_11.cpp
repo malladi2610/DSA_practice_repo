6cffcd1cb074a24d9f31d2899bf810e830ccee8a
Subhash <malladisubhash2610@gmail.com>
2024-09-30 18:38:36 +0200

    Added three solutions to the problem 11

--- 7.Array_problems/11.Missing_number/problem_11.cpp
+++ 7.Array_problems/11.Missing_number/problem_11.cpp
@@ -2,27 +2,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+
+/*Implementation using the hashmaps*/
+void find_missing_number(vector<int> &nums){
+    map<int, int> mp;
+    int missing = 0;
+    for(int i = 0; i<=nums.size(); i++){ //As it is a closed list
+        mp[nums[i]] = 1; 
+    }
+
+    for(int i = 0; i<=nums.size();i++){
+        if(mp.find(i) == mp.end()){
+            missing = i;
+            break;
+        }
+    }
+
+    cout<<"The missing number is :"<<missing;
+}
+
+
+
+/*The working implementation of the same logic (it takes o(n^2))*/
 void find_missing_number(vector<int> &nums){
     int missing = 0;
-    for(int i = 0; i<nums.size(); i++){
-
+    for(int i = 0; i<=nums.size(); i++){ //As it is a closed list
+        bool found = false;
         for(int j = 0; j<nums.size(); j++){
             if(nums[j] == i){
+                found = true;
                 break;
             }
-            else{
+        }
+
+        if(!found){
             missing = i;
-            }
-           
         }       
             
     }
-    // unordered_map<int, int> umap;
-    // for (int i=0;  i<nums.size(); i++){
-    //     if(umap[])
-    // }
     cout<<"The missing number is :"<<missing;
 }
+
+
+
+/*This code failed because if the missing of the bool variable that caused incorrect results*/
+// void find_missing_number(vector<int> &nums){
+//     int missing = 0;
+//     for(int i = 0; i<nums.size(); i++){
+
+//         for(int j = 0; j<nums.size(); j++){
+//             if(nums[j] == i){
+//                 break;
+//             }
+//             else{
+//             missing = i;
+//             }
+           
+//         }       
+            
+//     }
+//     cout<<"The missing number is :"<<missing;
+// }
 
 int main(){
     vector<int> nums;
